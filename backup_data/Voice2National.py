90aac4f2a4f3b7cf8f5367688b0eafe04b847e29
Gaurav Chopra <gaurav@intellewings.com>
2021-10-28 12:07:15 +0000

    DailyBackup:28-10-2021

---
+++ backup_data/Voice2National.py
@@ -0,0 +1,16 @@
+import pymongo
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+
+
+client = MongoClient('localhost', 27017)
+db = client['adverse_db']
+collection_batches = db['adverse_db']
+cursor = collection_batches.find({})
+
+for document in cursor:
+        if document["City of News Paper"] == "Voices":
+                print('found: ', document)
+                document["City of News Paper"] = "National"
+                collection_batches.save(document)
